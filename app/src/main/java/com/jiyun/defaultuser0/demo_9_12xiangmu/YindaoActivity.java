0cddc50f1ac78f01122dea49c9f6a1fed611c976
defaultuser0 <619923587@qq.com>
2017-09-12 19:23:15 +0800

    项目

---
+++ app/src/main/java/com/jiyun/defaultuser0/demo_9_12xiangmu/YindaoActivity.java
@@ -0,0 +1,15 @@
+package com.jiyun.defaultuser0.demo_9_12xiangmu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class YindaoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_yindao);
+        //启动页
+
+    }
+}
