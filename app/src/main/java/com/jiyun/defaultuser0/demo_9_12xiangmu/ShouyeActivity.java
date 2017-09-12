f6cabd16add5e5bd13b2b02fcd0aebb6b891d659
defaultuser0 <619923587@qq.com>
2017-09-12 19:08:29 +0800

    Initial commit

---
+++ app/src/main/java/com/jiyun/defaultuser0/demo_9_12xiangmu/ShouyeActivity.java
@@ -0,0 +1,14 @@
+package com.jiyun.defaultuser0.demo_9_12xiangmu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ShouyeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
---
+++ app/src/main/java/com/jiyun/defaultuser0/demo_9_12xiangmu/ZhuceActivity.java
@@ -0,0 +1,13 @@
+package com.jiyun.defaultuser0.demo_9_12xiangmu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ZhuceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shouye);
+    }
+}
